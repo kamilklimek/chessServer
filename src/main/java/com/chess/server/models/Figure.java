7395e75513e884088fa589c629243f6f3e70ad61
klimson <klimson@gitlab.com>
2018-04-06 22:05:21 +0200

    nvm commit

--- src/main/java/com/chess/server/models/Figure.java
+++ src/main/java/com/chess/server/models/Figure.java
@@ -140,8 +140,54 @@ public class Figure {
         return movements;
     }
 
-    protected List<Point> calculatePawnMovements(){
+    private List<Point> calculatePawnMovements(){
         List<Point> movements = new ArrayList<Point>();
+
+        int pawnPositionX = position.getPositionX();
+        int pawnPositionY = position.getPositionY();
+
+        List<Point> mask = new ArrayList<>(Arrays.asList(
+                new Point(0,1),
+                new Point(0,2),
+                new Point(1,1),
+                new Point(-1,1)
+        ));
+
+        //jezeli pionek ruszyl sie ze swojej pozycji to usun z maski mozliwosc ruchu o 2
+        if((pawnPositionY > 1 && isWhite) || (!isWhite && pawnPositionY < 6)){
+            mask.remove(new Point(0,2));
+        }
+
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+
+                if(isWhite){
+
+                    for(Point point : mask){
+                        boolean maskX = pawnPositionX + point.getPositionX() == j;
+                        boolean maskY = pawnPositionY + point.getPositionY() == i;
+
+                        if(maskX && maskY){
+                            movements.add(new Point(j,i));
+                        }
+                    }
+
+                }else{
+                    for(Point point : mask){
+                        boolean maskX = pawnPositionX + point.getPositionX() == j;
+                        boolean maskY = pawnPositionY - point.getPositionY() == i;
+
+                        if(maskX && maskY){
+                            movements.add(new Point(j,i));
+                        }
+                    }
+                }
+
+
+            }
+        }
+
+
         return movements;
     }
 
