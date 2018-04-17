b647a05435e4ea5941809e7e344f2dad8fc88a7a
klimson <klimson@gitlab.com>
2018-04-17 07:39:40 +0200

    Game object init

--- src/main/java/com/chess/server/game/Game.java
+++ src/main/java/com/chess/server/game/Game.java
@@ -1,9 +1,42 @@
 package com.chess.server.game;
 
-public class Game {
+import com.chess.server.figures.Figure;
+import com.chess.server.figures.Point;
 
-    Board board;
+import java.util.List;
 
+public class Game extends Board {
+
+    Player playerOne;
+    Player playerTwo;
+    boolean isTurnPlayersOne;
+
+    public Game(){
+        super();
+        playerOne = new Player("P1");
+        playerTwo = new Player("P2");
+        isTurnPlayersOne = true;
+    }
+
+    public boolean isTurnPlayerOne(){
+        return isTurnPlayersOne;
+    }
+
+    public boolean hasNextTurn(){
+        return isCheckMate(isTurnPlayersOne) ? false : true;
+    }
+
+    public void nextTurn(){
+        isTurnPlayersOne = !isTurnPlayersOne;
+    }
+
+    public int move(Point from, Point to){
+        return moveFigure(from, to);
+    }
+
+    private boolean isCheckMate(boolean isTurnPlayersOne){
+        return checkIsCheckMate(isTurnPlayersOne);
+    }
 
 
 }
